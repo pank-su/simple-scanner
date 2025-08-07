bdcc1255abac7609d12b1bff76e50ba020f5ad5f
pank-su <pank@pank.su>
2025-08-07 18:05:19 +0300

    :pencil2: Fix typo in preview function name

--- app/src/main/java/su/pank/simplescanner/ui/components/ScansCarousel.kt
+++ app/src/main/java/su/pank/simplescanner/ui/components/ScansCarousel.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import su.pank.simplescanner.R
 import su.pank.simplescanner.data.models.ScannedItem
@@ -209,7 +210,7 @@ fun EmptyScansCarouselPreview() {
 
 @Composable
 @DarkLightPreview
-fun LoadinScansCarouselPreview(){
+fun LoadingScansCarouselPreview(){
     SimpleScannerTheme {
         ScansCarousel(ScansUiState.Loading)
     }
