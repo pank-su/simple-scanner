2a696d566a326b7241c2d157148fa82086df7bfc
Pank <pank@pank.su>
2025-08-19 12:22:57 +0300

    :recycle: move coil deps in toml catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,8 @@ android {
 
 dependencies {
 
-    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 
 
 
